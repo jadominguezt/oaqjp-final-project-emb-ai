44f97dcafbefbec63df3d70df1c6a9828c2c0967
jadominguezt <jadominguezt@yahoo.com>
2026-02-20 15:53:05 -0500

    Commit 08

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+""" Main Server code for Flask """
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +6,7 @@ app = Flask("Emotion Detection")
 
 @app.route("/emotionDetector")
 def sent_emotion_detector():
+    """ Function that calls and retrieves the results from the emotion detector """
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
@@ -18,15 +20,21 @@ def sent_emotion_detector():
     sadness = response['sadness']
     dominant_emotion = response['dominant_emotion']
 
+    formatted_response = ""
+    if dominant_emotion is None:
+        formatted_response = "Invalid text! Please try again!."
+    else:
+        formatted_response = f"For the given statement, the system response is 'anger':\
+        {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}.\
+        The dominant emotion is {dominant_emotion}."
+
     # Return a formatted string with the sentiment label and score
-    return f"For the given statement, the system response is 'anger':\
-    {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}.\
-    The dominant emotion is {dominant_emotion}."
+    return formatted_response
 
 @app.route("/")
 def render_index_page():
+    """ Function to show the main website page """
     return render_template('index.html')
 
-    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
